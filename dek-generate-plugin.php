ec881d6eafa4e94d5ba839867899ade37759145a
Daniel Elias <danielek7@gmail.com>
2018-10-17 13:58:38 +1100

    initial commit

---
+++ dek-generate-plugin.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+ * @package DekGeneratePlugin
+ */
+
+/* 
+Plugin Name: Generate Plugin
+Plugin URI: https://generate.org.au
+Description: Integration with Podio that list Schools, Address, Chaplain, Position type with hours 
+Version: 1.0.0
+Author: Daniel Elias
+Author URI: https://github.com/delias
+License: MIT
+Text Domain: dek-generate-plugin
+*/
+
+/* 
+Copyright (c) 2018 Generate Ministries
+*/
+
+
+/* These functions work the same */
+/* 
+if ( ! defined( 'ABSPATH')) {
+  die;
+}
+ */
+
+ defined ( 'ABSPATH' ) or die ( 'Hey, you can\t access this file, you silly hacker!');
+
+/* 
+if ( ! function_exists('add_action')) {
+  echo 'Hey, you can\t access this file, you silly hacker!';
+  die;
+}
+ */
