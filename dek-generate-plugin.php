b287e100b0ed115b503b7e15675c8bfa8628f370
Daniel Elias <danielek7@gmail.com>
2018-10-17 15:56:44 +1100

    Update dek-generate-plugin.php

--- dek-generate-plugin.php
+++ dek-generate-plugin.php
@@ -25,12 +25,50 @@ if ( ! defined( 'ABSPATH')) {
   die;
 }
  */
-
- defined ( 'ABSPATH' ) or die ( 'Hey, you can\t access this file, you silly hacker!');
-
 /* 
 if ( ! function_exists('add_action')) {
   echo 'Hey, you can\t access this file, you silly hacker!';
   die;
 }
- */
+*/
+
+defined ( 'ABSPATH' ) or die ( 'Hey, you can\t access this file, you silly hacker!');
+
+class DekGeneratePlugin
+{
+  function __construct(){
+    add_action( 'init', array( $this, 'custom_post_type'));
+  }
+
+  function activate(){
+    // generated a CPT
+    // $this->custom_post_type();
+    // flush rewrite rules
+    flush_rewrite_rules();
+  }
+  function deactivate(){
+    // flush rewrite rules
+    
+  }
+  function uninstall(){
+    // delete CPT
+    // delete all the plugin data from the DB
+    
+  }
+
+  function custom_post_type(){
+    register_post_type( 'schools', ['public' => true, 'label' => 'Schools']);
+  }
+}
+
+if ( class_exists('DekGeneratePlugin')) {
+  $dekGeneratePlugin = new DekGeneratePlugin();
+}
+
+// activation
+register_activation_hook( __FILE__, array($dekGeneratePlugin, 'activate'));
+
+// deactivation
+register_deactivation_hook( __FILE__, array($dekGeneratePlugin, 'deactivate'));
+
+// uninstall
